ded4b0bb8bf5a7f1feebd110b4c90e29b7bb4bc0
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2017-11-04 11:17:09 -0200

    Criação da Interface IUsuariosRepositorio.cs.

---
+++ SistemaDeVoluntarios/SistemaDeVoluntarios.Dominio/Repositorios/IUsuariosRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaDeVoluntarios.Dominio.Entidades;
+
+namespace SistemaDeVoluntarios.Dominio.Repositorios
+{
+    public interface IUsuariosRepositorio
+    {
+        void Inserir(Usuarios usuarios);
+        void Alterar(Usuarios usuarios, string id);
+        void Excluir(string id);
+        Usuarios Procurar(Guid id);
+    }
+}
