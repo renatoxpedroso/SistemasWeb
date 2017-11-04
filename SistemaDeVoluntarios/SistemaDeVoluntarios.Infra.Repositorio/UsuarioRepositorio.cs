b290cf5daecd79c952ffdedafd969e8f758b1300
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2017-11-04 16:42:39 -0200

    Modificações no UsuarioRepositorio.cs

--- SistemaDeVoluntarios/SistemaDeVoluntarios.Infra.Repositorio/UsuarioRepositorio.cs
+++ SistemaDeVoluntarios/SistemaDeVoluntarios.Infra.Repositorio/UsuarioRepositorio.cs
@@ -4,10 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SistemaDeVoluntarios.Dominio.Entidades;
+using SistemaDeVoluntarios.Dominio.Repositorios;
+using Npgsql;
+using System.Data;
+
 namespace SistemaDeVoluntarios.Infra.Repositorio
 {
-    class UsuarioRepositorio
+    public class UsuarioRepositorio : IUsuariosRepositorio
     {
-        //Renato
+        public string strConexao;
+
+        public UsuarioRepositorio(string strConexao)
+        {
+            this.strConexao = strConexao;
+        }
+
+        public void Inserir(Usuarios usuarios)
+        {
+            using(NpgsqlConnection con = new NpgsqlConnection(strConexao))
+            {
+                con.Open();
+                NpgsqlCommand comando = new NpgsqlCommand();
+                comando.Connection = con;
+                comando.CommandText = "Insert into usuarios (CodUsuario, TipoUsuario, TipoPessoa, Nome, Email, Senha, DataNacimento, cpfCnpj, Telefone, Celular, Rua, Numero, Bairro, Cidade, Cep, Estado) " +
+                    "Values(@CodUsuario, @TipoUsuario, @TipoPessoa, @Nome, @Email, @Senha, @DataNacimento, @cpfCnpj, @Telefone, @Celular, @Rua, @Numero, @Bairro, @Cidade, @Cep, @Estado);";
+
+                comando.Parameters.AddWithValue("CodUsuario", usuarios.CodUsuario);
+
+            }
+        }
+
+        public void Alterar(Usuarios usuarios, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuarios Procurar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public 
     }
 }
