78838d2e8b5e9780af5ea8aefb7893000cd03c17
renatoxpedroso <renatoxpedroso@gmail.com>
2017-11-13 21:07:30 -0200

    Implementação de cadastro de usuarios.

---
+++ SistemaDeVoluntarios/SistemaDeVoluntarios.Aplicacao/UsuarioAplicacao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaDeVoluntarios.Dominio.Entidades;
+using SistemaDeVoluntarios.Dominio.Repositorios;
+
+
+namespace SistemaDeVoluntarios.Aplicacao
+{
+    public class UsuarioAplicacao
+    {
+        private IUsuariosRepositorio usuarioRepositorio;
+        public UsuarioAplicacao(IUsuariosRepositorio usuarioRepositorio)
+        {
+            this.usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public void Inserir(Usuarios usuarios)
+        {
+            if (string.IsNullOrEmpty(usuarios.Nome))
+            {
+                throw new Exception("O produto está sem nome!");
+            }
+
+            this.usuarioRepositorio.Inserir(usuarios);
+        }
+    }
+}
